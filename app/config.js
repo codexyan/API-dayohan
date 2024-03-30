b35529563212543b7b15eab8c437f326172d8451
codexyan <mdcodeid@gmail.com>
2024-03-31 06:03:15 +0700

    update config

--- app/config.js
+++ app/config.js
@@ -3,8 +3,8 @@ dotenv.config();
 
 module.exports = {
   urlDb: process.env.URL_MONGODB_DEV,
-  jwtExpiration: "24h",
-  jwtSecret: "jwtSecret",
+  jwtExpiration: process.env.JWT_EXPIRATION || "24h",
+  jwtSecret: process.env.JWT_SECRET_KEY || "jwtSecret",
   gmail: process.env.GMAIL,
   password: process.env.PASSWORD,
 };
